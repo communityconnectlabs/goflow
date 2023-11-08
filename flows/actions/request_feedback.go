e560670e1342d169a51242474210a479a6290257
Ihor Palii <palii.ihor@uvik.net>
2023-11-08 23:18:23 +0200

    Fixed doc gen test

--- flows/actions/request_feedback.go
+++ flows/actions/request_feedback.go
@@ -19,12 +19,12 @@ const TypeRequestFeedback string = "request_feedback"
 //
 // An [event:feedback_requested] event will be created if the form could be sent.
 //
-//	{
-//	  "uuid": "8eebd020-1af5-431c-b943-aa670fc74da9",
-//	  "type": "request_feedback",
-//	  "star_rating_question": "How would you rate us?",
-//	  "comment_question": "Please, leave a comment."
-//	}
+//   {
+//     "uuid": "8eebd020-1af5-431c-b943-aa670fc74da9",
+//     "type": "request_feedback",
+//     "star_rating_question": "How would you rate us?",
+//     "comment_question": "Please, leave a comment."
+//   }
 //
 // @action request_feedback
 type RequestFeedbackAction struct {
